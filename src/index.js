9e613bc88a7cf05096a2962dccef283b892471f2
tackt21 <82143268+takutonakagawa@users.noreply.github.com>
2021-08-30 08:55:59 +0900

    add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const nameArr = ["ハルヒ", "ひたぎ", "タクト"];
+
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// };
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+
+// console.log(nameArr2);
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "タクト") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+
+// console.log(newNameArr);
+
+// 三項演算子
+const num = 1300;
+// console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+};
+console.log(checkSum(50, 30));
