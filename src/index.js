d44d466ee92fe8ca1dd729ee9e629c2301592d75
tackt21 <82143268+takutonakagawa@users.noreply.github.com>
2021-08-31 22:27:07 +0900

    appendChild

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import "./styles.css";
 
 const onClickAdd = () => {
+  // テキストボックスのあたいを取得し、初期化する
   const inputText = document.getElementById("add-text").value;
-  // console.log(inputText);
+  document.getElementById("add-text").value = "";
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+  // li生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  // 未完成リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
