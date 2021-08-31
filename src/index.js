971b8b3cff0c91febfcefa5d07d2528d847619a8
tackt21 <82143268+takutonakagawa@users.noreply.github.com>
2021-08-31 22:35:40 +0900

    buttonCreateFunction

--- src/index.js
+++ src/index.js
@@ -7,14 +7,26 @@ const onClickAdd = () => {
   // div生成
   const div = document.createElement("div");
   div.className = "list-row";
-  console.log(div);
+
   // li生成
   const li = document.createElement("li");
   li.innerText = inputText;
-  // divタグの子要素に各要素を設定
-  div.appendChild(li);
+
   // 未完成リストに追加
   document.getElementById("incomplete-list").appendChild(div);
+
+  // 完了ボタンの作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+
+  // 削除ボタンの作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 };
 
 document
